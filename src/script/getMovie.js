7232b0c9b78cb0518a2424487320c6f91214e36b
h0ngwon <w___on@naver.com>
2023-10-18 14:43:45 +0900

    Feat: TMDB 데이터 불러오는 기능

---
+++ src/script/getMovie.js
@@ -0,0 +1,41 @@
+const searchButton = document.querySelector('.search-btn');
+
+// button click test
+const searchBtnClick = (e) => {
+	e.preventDefault();
+	console.log('click');
+};
+
+// fetch option for TMDB
+const options = {
+	method: 'GET',
+	headers: {
+		accept: 'application/json',
+		Authorization:
+			'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOWUyMGU0ZmIxNTBiZDJmMWI0MjVmMjIxNWE2YmRjYiIsInN1YiI6IjYxZmJhNjlhY2I4MDI4NGFjZmYzYTk1OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.s0qfKixPyi99zIoSgu_oryvWZFRdCJp2ffSoPC-VCX0',
+	},
+};
+
+const getMovieData = async () => {
+	const response = await fetch(
+		'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',
+		options
+	);
+
+	const data = await response.json();
+	console.log(data);
+};
+
+// get movie data function
+const getFilteredTopRatedMovie = async () => {
+	const response = await fetch(
+		'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',
+		options
+	);
+
+	const data = await response.json();
+	console.log(data);
+};
+
+searchButton.addEventListener('click', getFilteredTopRatedMovie);
+window.onload = getMovieData;
