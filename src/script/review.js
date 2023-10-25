1e7a14f91d5b27cca1afb4a893b0a520b3243168
h0ngwon <w___on@naver.com>
2023-10-25 21:42:15 +0900

    Style: 불필요한 공백 제거 및 스페이스 변경

--- src/script/review.js
+++ src/script/review.js
@@ -1,46 +1,46 @@
-const reviewForm = document.querySelector('.review-form');
-const userName = document.getElementById('name');
-const password = document.getElementById('password');
-const rating = document.getElementById('rating');
-const review = document.getElementById('review');
+const reviewForm = document.querySelector(".review-form");
+const userName = document.getElementById("name");
+const password = document.getElementById("password");
+const rating = document.getElementById("rating");
+const review = document.getElementById("review");
 
-const reviewGroup = document.querySelector('.review-group');
-const deleteBtn = document.querySelectorAll('.delete-btn');
+const reviewGroup = document.querySelector(".review-group");
+const deleteBtn = document.querySelectorAll(".delete-btn");
 
-let storageMovieId = localStorage.getItem('movieId');
+let storageMovieId = localStorage.getItem("movieId");
 console.log(storageMovieId);
 let storage = JSON.parse(localStorage.getItem(`${storageMovieId}`)) || [];
 let id = storage.length;
 
 // 새 리뷰
 const submitReview = (e) => {
-  e.preventDefault();
-  const userReview = {
-    name: userName.value,
-    password: password.value,
-    rating: rating.value,
-    review: review.value,
-    id: id++,
-  };
-  storage.push(userReview);
-  reviewForm.reset();
-  saveReview();
+	e.preventDefault();
+	const userReview = {
+		name: userName.value,
+		password: password.value,
+		rating: rating.value,
+		review: review.value,
+		id: id++,
+	};
+	storage.push(userReview);
+	reviewForm.reset();
+	saveReview();
 };
 
 // 리뷰 저장
 const saveReview = () => {
-  localStorage.setItem(
-    `${localStorage.getItem('movieId')}`,
-    JSON.stringify(storage)
-  );
-  getReview();
+	localStorage.setItem(
+		`${localStorage.getItem("movieId")}`,
+		JSON.stringify(storage)
+	);
+	getReview();
 };
 
 // 리뷰 가져오기
 const getReview = () => {
-  const reviewCard = storage
-    .map((review) => {
-      return `
+	const reviewCard = storage
+		.map((review) => {
+			return `
         <div>
           <span>${review.name}(${review.rating})</span>
           <button>수정</button>
@@ -50,17 +50,17 @@ const getReview = () => {
           </p>
         </div>
   `;
-    })
-    .join('');
-  reviewGroup.innerHTML = reviewCard;
+		})
+		.join("");
+	reviewGroup.innerHTML = reviewCard;
 };
 
 // 리뷰 삭제
 const deleteReview = (id) => {
-  storage = storage.filter((review) => review.id !== id);
-  saveReview();
+	storage = storage.filter((review) => review.id !== id);
+	saveReview();
 };
 
 getReview();
 
-reviewForm.addEventListener('submit', submitReview);
+reviewForm.addEventListener("submit", submitReview);
